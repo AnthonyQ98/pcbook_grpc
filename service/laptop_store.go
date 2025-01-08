3be46b72a5403e86559f561ce313e512198b1da5
Anthony Quinn <anthonyquinn.developer@gmail.com>
2025-01-08 19:38:04 +0000

    implement store for new server-side streaming message

--- service/laptop_store.go
+++ service/laptop_store.go
@@ -14,6 +14,7 @@ var ErrAlreadyExists = errors.New("record already exists")
 type LaptopStore interface {
 	Save(laptop *pb.Laptop) error
 	Find(id string) (*pb.Laptop, error)
+	Search(filter *pb.Filter, found func(laptop *pb.Laptop) error) error
 }
 
 type InMemoryLaptopStore struct {
@@ -54,6 +55,68 @@ func (store *InMemoryLaptopStore) Find(id string) (*pb.Laptop, error) {
 	}
 
 	return deepCopy(laptop)
+}
+
+func (store *InMemoryLaptopStore) Search(filter *pb.Filter, found func(laptop *pb.Laptop) error) error {
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
+
+	for _, laptop := range store.data {
+		if isQualified(filter, laptop) {
+			other, err := deepCopy(laptop)
+			if err != nil {
+				return err
+			}
+
+			err = found(other)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func isQualified(filter *pb.Filter, laptop *pb.Laptop) bool {
+	if laptop.GetPriceUsd() > filter.GetMaxPriceUsd() {
+		return false
+	}
+
+	if laptop.GetCpu().NumberCores < filter.GetMinCpuCores() {
+		return false
+	}
+
+	if float64(laptop.GetCpu().GetMinGhz()) < filter.GetMinCpuGhz() {
+		return false
+	}
+
+	if toBit(laptop.GetRam()) < toBit(filter.GetMinRam()) {
+		return false
+	}
+
+	return true
+}
+
+func toBit(memory *pb.Memory) uint64 {
+	value := memory.GetValue()
+
+	switch memory.GetUnit() {
+	case pb.Memory_BIT:
+		return value
+	case pb.Memory_BYTE:
+		return value << 8
+	case pb.Memory_KILOBYTE:
+		return value << 13
+	case pb.Memory_MEGABYTE:
+		return value << 23
+	case pb.Memory_GIGABYTE:
+		return value << 33
+	case pb.Memory_TERABYTE:
+		return value << 43
+	default:
+		return 0
+
+	}
 }
 
 func deepCopy(laptop *pb.Laptop) (*pb.Laptop, error) {
